5aa624fcd276f2a713051ff8d5ae38eb118ef961
marceloacunaosorio <m.acunao@duocuc.cl>
2024-06-23 19:42:21 -0400

    se agrega footer

--- Pro_Angular001/src/app/app.component.ts
+++ Pro_Angular001/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './component/menu/menu.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MenuComponent
+    MenuComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

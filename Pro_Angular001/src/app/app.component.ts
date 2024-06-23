9a7e3e64005a0e464b12b32f670152cccc782f08
marceloacunaosorio <m.acunao@duocuc.cl>
2024-06-23 19:15:33 -0400

    se agrega carrucel a la pagina principal

--- Pro_Angular001/src/app/app.component.ts
+++ Pro_Angular001/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './component/menu/menu.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MenuComponent
+    MenuComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

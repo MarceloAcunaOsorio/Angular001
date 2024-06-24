84f9ae0a99f9ff4be9154d2e82d2c6db33fcafc3
marceloacunaosorio <m.acunao@duocuc.cl>
2024-06-23 23:47:51 -0400

    se cargan los productos en cada categoria

--- Pro_Angular001/src/app/Data/datos.ts
+++ Pro_Angular001/src/app/Data/datos.ts
@@ -10,6 +10,46 @@ export const listaProdutosNike: Producto[]=[
     {id:7, nombre:'productoNike7',precio:599, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaNike1.jpg'},
     {id:8, nombre:'productoNike8',precio:299, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaNike1.jpg'}
 ];
+
+
+
+export const listaProdutosPuma: Producto[]=[
+    {id:1, nombre:'productoPuma1',precio:100, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaPuma1.jpg'},
+    {id:2, nombre:'productoPuma2',precio:150, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaPuma1.jpg'},
+    {id:3, nombre:'productoPuma3',precio:300, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaPuma1.jpg'},
+    {id:4, nombre:'productoPuma4',precio:110, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaPuma1.jpg'},
+    {id:5, nombre:'productoPuma5',precio:500, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaPuma1.jpg'},
+    {id:6, nombre:'productoPuma6',precio:220, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaPuma1.jpg'},
+    {id:7, nombre:'productoPuma7',precio:599, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaPuma1.jpg'}
+];
+
+
+
+export const listaProdutosAdidas: Producto[]=[
+    {id:1, nombre:'productoAdidas1',precio:100, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaAdidas1.jpg'},
+    {id:2, nombre:'productoAdidas2',precio:150, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaAdidas1.jpg'},
+    {id:3, nombre:'productoAdidas3',precio:300, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaAdidas1.jpg'},
+    {id:4, nombre:'productoAdidas4',precio:110, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaAdidas1.jpg'},
+    {id:5, nombre:'productoAdidas5',precio:500, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaAdidas1.jpg'},
+    {id:6, nombre:'productoAdidas6',precio:220, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaAdidas1.jpg'},
+    {id:7, nombre:'productoAdidas7',precio:599, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaAdidas1.jpg'},
+    {id:8, nombre:'productoAdidas8',precio:599, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaAdidas1.jpg'},
+    {id:9, nombre:'productoAdidas9',precio:599, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillaAdidas1.jpg'}
+];
+
+
+export const listaProdutosReebok: Producto[]=[
+    {id:1, nombre:'productoReebok1',precio:100, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillasReebok1.jpg'},
+    {id:2, nombre:'productoReebok2',precio:150, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillasReebok1.jpg'},
+    {id:3, nombre:'productoReebok3',precio:300, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillasReebok1.jpg'},
+    {id:4, nombre:'productoReebok4',precio:110, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillasReebok1.jpg'},
+    {id:5, nombre:'productoReebok5',precio:500, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillasReebok1.jpg'},
+    {id:6, nombre:'productoReebok6',precio:220, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillasReebok1.jpg'},
+    {id:7, nombre:'productoReebok7',precio:599, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillasReebok1.jpg'},
+    {id:8, nombre:'productoReebok8',precio:599, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillasReebok1.jpg'},
+    {id:9, nombre:'productoReebok9',precio:599, stock:10, descripcion:'zapatillas deportivas',imagen:'assets/img/zapatillasReebok1.jpg'}
+];
+
 export const listaItems=['nike','puma','adidas','reebok'];
 
 
